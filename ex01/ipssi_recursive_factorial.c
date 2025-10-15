5e2c0db096c3e580c35679c6d59aa2cc562b75b3
KevinjCode <megbart@msn.com>
2025-10-15 14:15:08 +0200

    norminette ex01

--- ex01/ipssi_recursive_factorial.c
+++ ex01/ipssi_recursive_factorial.c
@@ -1,5 +1,4 @@
-#include<unistd.h>
-
+#include <unistd.h>
 
 int	ipssi_recursive_factorial(int nb)
 {
@@ -7,14 +6,12 @@ int	ipssi_recursive_factorial(int nb)
 	{
 		return (0);
 	}
-
 	if (nb == 0)
 	{
-		return(1);
+		return (1);
 	}
-	return nb * ipssi_recursive_factorial(nb - 1);
-	}
-
+	return (nb * ipssi_recursive_factorial(nb - 1));
+}
 
 void	ipssi_putchar(char c)
 {
@@ -26,19 +23,11 @@ void	ipssi_putnbr(int nbr)
 	if (nbr < 0)
 	{
 		nbr = -nbr;
-		ipssi_putchar('-');
+		ipssi_putchar ('-');
 	}
 	if (nbr > 9)
 	{
 		ipssi_putnbr(nbr / 10);
 	}
 	ipssi_putchar(nbr % 10 + '0');
-}
-
-int main(void)
-{
-    int result = ipssi_recursive_factorial(12);
-    ipssi_putnbr(result);
-    ipssi_putchar('\n');
-    return 0;
 }
