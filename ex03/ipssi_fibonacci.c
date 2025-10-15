9b4273acc88eae1e963d72962bca7143e82940f1
KevinjCode <megbart@msn.com>
2025-10-15 15:24:18 +0200

    Ajout ex03

---
+++ ex03/ipssi_fibonacci.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int	ipssi_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		return (0);
+	}
+	if (index == 1)
+	{
+		return (1);
+	}
+	return (ipssi_fibonacci(index - 1) + ipssi_fibonacci(index - 2));
+}
