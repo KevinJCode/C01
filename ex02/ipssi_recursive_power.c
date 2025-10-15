940814d70daf303c2c2df0e5f2592691b0d356b0
KevinjCode <megbart@msn.com>
2025-10-15 14:30:43 +0200

    Ajout ex02

---
+++ ex02/ipssi_recursive_power.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+int	ipssi_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * ipssi_recursive_power(nb, power - 1));
+}
